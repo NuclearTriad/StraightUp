7aebd501c1ed47ec897fb358a3d77f1c960160d3
PinziPictures <32314235+PinziPictures@users.noreply.github.com>
2018-01-23 18:21:54 +0100

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -17,6 +17,9 @@ var sequoiaDemoOn=false;
 var burjDemoOn=false;
 var demoTitlesOn=false;
 
+var check_scal=false; //la scalata è iniziata, si può iniziare il conteggio dei metri
+var scelto=-1;//id globale dell'edificio scelto
+
 var myData, //segnaposto JSON
 
     posRelMe = [], //Oggetto Posizioni oggetti rispetto alla mia posizione
@@ -47,7 +50,34 @@ var myData, //segnaposto JSON
     heightLink = [];
 
     hit_struct = [];
+    
+    //variabili scalata+geolocalizzazione
+    
+var latitude,
+    longitude,
+    accuracy,
 
+    numeroAgg = -1,
+    metriTOT = 0,
+    metriPrec = 0,
+    veli = 0,
+    tempo = 0,
+    backUpPositionLat = [],
+    backUpPositionLon = [],
+    backUpPositionDist = [],
+
+    stabilizzato = false, //inizia con la propriteà non stabilizzata
+    backUpstabilizzation = [], //crea la array dei valori per stabilizzare
+    stabilizzationTOT = 0,
+    accuracyLimit = 0.2, //valore in metri che deve avere la sommatoria della array precedente per essere considerata accettabile
+    maxStabilizzationArray = 4, //massimo numero di valori che l'array di sopra può tenere (maggiore è più preciso è)
+
+    conv=0, //conversione da m in pixel di scalata
+
+    imgMask,
+
+    position,
+    geoLoc;
 
 function preload() { //tutti i preload delle immagini e i font
   console.time('preload');
@@ -74,13 +104,15 @@ function preload() { //tutti i preload delle immagini e i font
 }
 
 function setup() { //tutti i default dell'interfaccia
+  mask = createGraphics(720, 1280); //crea il segnaposto per la mascherma sotto (le grandezze qui si ripetono poi sotto)
+    
   createCanvas(innerWidth,innerHeight);
   rectMode(CENTER);
   textAlign(CENTER);
   angleMode(DEGREES);
   textFont(ubuntuMediumItalic);
 
-  //getLocationUpdate()
+  getLocationUpdate()
   calcPosRelMe()
 
   for (var i=0; i < myData.landmarks_en.length; i++) {
@@ -104,7 +136,7 @@ function setup() { //tutti i default dell'interfaccia
   }
 
 function draw() {
-
+    
   if(mouseIsPressed==false) {mouseX=-100; mouseY=-100;}
 
   translate(width/2,height/2);
@@ -135,19 +167,25 @@ function draw() {
   };
 
   if(sequoiaDemoOn==true) { //avvia la modalità scalata (climbOn viene impostato come true solo dopo la pressione del pulsante della squoia, per ora)
+    check_scal=true;
+    scelto=7;
     climbMode(7,true,1.25,1.25,-700,200); //structNum,cloudBool,cloudX,cloudY,cloudMin,cloudMax
   };
 
   if(burjDemoOn==true) { //avvia la modalità scalata (climbOn viene impostato come true solo dopo la pressione del pulsante della squoia, per ora)
+    check_scal=true;
+    scelto=8;
     climbMode(8,true,15,2,-550,400); //structNum,cloudBool,cloudX,cloudY,cloudMin,cloudMax
   };
 
   for (var i=0; i < myData.landmarks_en.length; i++){
       if(hit_struct[i]==true){
+          check_scal=true;
           radarOn=false;
           console.log("cliccato "+i);
           push();
           //translate(0,-posYPointer) //counter posYPointer
+          scelto=i;
           climbMode(i,true,2,1.25,-700,200); //structNum,cloudBool,cloudX,cloudY,cloudMin,cloudMax
           pop();
         };
@@ -156,7 +194,14 @@ function draw() {
 if(backMenu==true) { //se true fa comparire il menu per tornare indietro
   bMenu();
 }
-
+  push();
+  text('latitude: ' + latitude, 0, 30);
+  text('longitude: ' + longitude, 0, 30 * 2);
+  text('stabile: ' + stabilizzato + scelto, 0, 30 * 3);
+  text('accuracy: ' + accuracy, 0, 30 * 4);
+  text('Aggiornamenti: ' + numeroAgg, 0, 30 * 5);
+  text('Distanza Precedente: ' + metriPrec, 0, 30 * 6);
+  pop();
   // console.log('infoOn: '+infoOn);
   // console.log('infoButtonShow: '+infoButtonShow);
 } //draw END
@@ -319,7 +364,9 @@ var f=300;
 function climbMode(structNum,cloudBool,cloudX,cloudY,cloudMin,cloudMax) { //structNum,cloudBool,cloudX,cloudY,cloudMin,cloudMax
   radarOn=false;
   climbOn=true;
-
+  
+  
+    
   if(f>1800){f=1800} else{f+=50;}
   if(f>1000){
     demoTitlesOn=false;
@@ -327,7 +374,10 @@ function climbMode(structNum,cloudBool,cloudX,cloudY,cloudMin,cloudMax) { //stru
     climbStructure(structNum,cloudBool,cloudX,cloudY,cloudMin,cloudMax);
     climbInterface(structNum);
     backArrow();
-    completed();
+    if(metriTOT>=heightLink[structNum]){
+        check_scal=false;
+        completed();
+    }
       if(infoOn==true) { //se true fa comparire la schermata con le informazioni sulla struttura
         setTimeout(infoScreen(structNum),400);
         if(infoButtonShow==true){infoButton()};
@@ -337,10 +387,10 @@ function climbMode(structNum,cloudBool,cloudX,cloudY,cloudMin,cloudMax) { //stru
   rectMode(CORNER);
   fill(colorList[0]);
   noStroke();
-
+    
   rect(width,height-f,-width*2,800);
   pop();
-
+    
 
 };
 
@@ -352,7 +402,7 @@ function climbInterface(structNum) {
   textFont(ubuntuBoldItalic);
   textSize(40);
   fill(colorList[5]);
-  text(heightLink[structNum]+'m',0,0);
+  text(Math.round(metriTOT*10)/10+'/'+myData.landmarks_en[scelto].height+'m',0,0);
   pop();
 }
 var cloudSwitch=false;
@@ -369,11 +419,11 @@ function climbStructure(structNum,cloudBool,cloudX,cloudY,cloudMin,cloudMax) {
   c+=0.1;
   if(c>cloudMax) {c=cloudMin};
   scale(0.5);
-  image(cloud,width/cloudX+c,-height/cloudY);
+  //image(cloud,width/cloudX+c,-height/cloudY);
   pop();
   }
 
-  //background
+  background
   push();
   if(innerHeight<=512) {
   translate(0,-height/8.3);
@@ -382,6 +432,7 @@ else{
   translate(0,-height/40);
 }
   scale(width/720);
+  
   image(imgLinkBack[structNum],0,0);
   pop();
 
@@ -395,6 +446,9 @@ else{
   scale(width/850);
 }
   translate(0,height/6);
+  //mask.rect(0, 1280-conv, 720, 1280); //crea maschera da rettangolo
+  //( imgClone = imgLink[structNum].get() ).mask( mask.get() );
+  //image(imgClone,0,-height/40);
   image(imgLink[structNum],0,-height/40);
   pop();
 
@@ -826,6 +880,10 @@ rectMode(CORNER);
 // rect(width/16,-height/11,width/4.7,height/10);
 hit_yes = collidePointRect(mouseX-width/2,mouseY-height/2,-width/3.7,-height/11,width/4.7,height/10);
 if(hit_yes==true) {
+  metriTOT=0;
+  scelto=-1;
+  backUpPositionDist=[];
+  check_scal=false;
   if(sequoiaDemoOn==false && burjDemoOn==false) {
     setTimeout(function() {
     burjDemoOn=false;
@@ -973,67 +1031,100 @@ function zoomOut() {
 
 //Funzioni di Geolocalizzazione più opizioni
 
-function getLocationUpdate() {
-  if (navigator.geolocation) {
-    var options = {
-      timeout:60000,
-      maximumAge:10000,
-      enableHighAccuracy: true};
+function stabilizzation() {
+  if (stabilizzato==false) { //Stabilizzazione
+    if (isNaN(metriPrec)==false) {backUpstabilizzation.push(metriPrec);} //se la distanza è un valore numerico mettila nell Array della stabilizzazione
+    if (backUpstabilizzation.length>maxStabilizzationArray) {backUpstabilizzation.shift()}
+    if ((backUpstabilizzation.length==4)&&(stabilizzationTOT<accuracyLimit)) {stabilizzato=true; console.log("stabilizzato")}; //se la sommatoria delle
 
-      geoLoc = navigator.geolocation;
-      watchID = geoLoc.watchPosition(showLocation, errorHandler, options);
-    }
-
-  else{alert("Sorry, this browser does not support geolocation!");}
+    stabilizzationTOT = backUpstabilizzation.sum();
+  }
 }
+
+// Funzioni per chiamare getLocationUpdate() (funziona anche senza chiamere )
 
 function showLocation(position) {
-  myLat = position.coords.latitude;
-  myLon = position.coords.longitude;
+    latitude = position.coords.latitude; //prendi la latitudine dell'utente
+    longitude = position.coords.longitude; //prendi la longitudine dell'utente
+    accuracy = position.coords.accuracy; //prendi l'accuratezza della precisione dell'utente
+    heading = position.coords.heading; //prendi la direzione rispetto al nord dell'utente
 
-  if (isNaN(position.coords.heading)==false) {heading = position.coords.heading;} //definisci l'heading solo se è un numero, altrimenti è 0.
-  else {heading = 0;}
+    numeroAgg++
 
-  calcPosRelMe()
-}
+    backUpPositionLat.push(latitude); //aggiungi la latitudine alla Array di backup delle latitudini
+    backUpPositionLon.push(longitude); //aggiungi la longitudine alla Array di backup delle longitudini
+
+    metriPrec = measure(backUpPositionLat[numeroAgg],backUpPositionLon[numeroAgg],backUpPositionLat[numeroAgg-1],backUpPositionLon[numeroAgg-1]) //calcola la distanza tra la posizione precedente è quella attuale
+    metriPrec = Math.round(metriPrec*100)/100 //arrotonda la distanza precedente
+
+    stabilizzation() //Stabilizzazione
+    if(scelto!=-1){
+        if ((stabilizzato==true)&&(metriTOT<myData.landmarks_en[scelto].height)&&(metriPrec>accuracyLimit*2)&&check_scal==true) {
+
+          if (isNaN(metriPrec)==false) {backUpPositionDist.push(metriPrec);} //se gli aggiornamenti hanno raggiunto la quota di 15. inizia ad aggiungere le distanze percorse alla Array di tutte le distanze
+          metriTOT = backUpPositionDist.sum(); //fai la sommatoria della Array di tutte le distanze percorse per sapere la distanza totale percorsa
+
+          conv = map(metriTOT, 0, myData.landmarks_en[scelto].height, 0, myData.landmarks_en[scelto].hPx); //converte la distanza in m in pixel di scalata
+        }
+    }
+    
+  }
 
 function errorHandler(err) {
-  if (err.code == 1) {
-    alert("Error: Access is denied!");
-   }
+    if (err.code == 1) {
+      alert("Error: Access is denied!");
+     }
 
-  else if ( err.code == 2) {
-    alert("Error: Position is unavailable!");
+    else if ( err.code == 2) {
+      alert("Error: Position is unavailable!");
+    }
+
+    else if ( err.code == 3) {
+      alert("Error: Timeout");
+    }
+
+    else if ( err.code == 0) {
+      alert("Error: an unkown error occurred");
+    }
   }
 
-  else if ( err.code == 3) {
-    alert("Error: Timeout");
-  }
+function getLocationUpdate(){
+    if(navigator.geolocation){
+     // timeout at 60000 milliseconds (60 seconds)
+    var options = {
+     timeout:60000,
+     maximumAge:10000,
+     enableHighAccuracy: true};
 
-  else if ( err.code == 0) {
-    alert("Error: an unkown error occurred");
-  }
-}
+     geoLoc = navigator.geolocation;
+     watchID = geoLoc.watchPosition(showLocation, errorHandler, options);
+    }
 
-function stopWatch(){geoLoc.clearWatch(watchID);}
+    else{
+      alert("Sorry, browser does not support geolocation!");
+     }
 
 
-//Function Aggiuntive
+    }
+
+    function stopWatch(){
+     geoLoc.clearWatch(watchID);
+    }
 
 function measure(lat1, lon1, lat2, lon2) {  // generally used geo measurement function
-    var R = 6378.137; // Radius of earth in KM
-    var dLat = lat2 * Math.PI / 180 - lat1 * Math.PI / 180;
-    var dLon = lon2 * Math.PI / 180 - lon1 * Math.PI / 180;
-    var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
-    Math.cos(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180) *
-    Math.sin(dLon/2) * Math.sin(dLon/2);
-    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-    var d = R * c;
-    return d * 1000; // meters
-  }
+      var R = 6378.137; // Radius of earth in KM
+      var dLat = lat2 * Math.PI / 180 - lat1 * Math.PI / 180;
+      var dLon = lon2 * Math.PI / 180 - lon1 * Math.PI / 180;
+      var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+      Math.cos(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180) *
+      Math.sin(dLon/2) * Math.sin(dLon/2);
+      var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+      var d = R * c;
+      return d * 1000; // meters
+    }
 
 Array.prototype.sum = function() {
-  var total = 0;
-  for(var i = 0; i < this.length; i += 1) {total += this[i];}
-  return total;
-};
+		    var total = 0;
+		    for(var i = 0; i < this.length; i += 1) {total += this[i];}
+		    return total;
+	  };
