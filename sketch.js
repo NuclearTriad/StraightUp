1a40a26553be7b93cc2e050f76dfa66a727e1857
EverSinceWWI <32314108+EverSinceWWI@users.noreply.github.com>
2018-02-03 22:43:52 +0100

    fix demo

--- sketch.js
+++ sketch.js
@@ -183,14 +183,14 @@ function draw() {
 
   if(sequoiaDemoOn==true) { //avvia la modalità scalata (climbOn viene impostato come true solo dopo la pressione del pulsante della squoia, per ora)
     //check_scal=true;
-    scelto=7;
-    climbMode(7,true,1.25,1.25,-700,200); //structNum,cloudBool,cloudX,cloudY,cloudMin,cloudMax
+    scelto=8;
+    climbMode(8,true,1.25,1.25,-700,200); //structNum,cloudBool,cloudX,cloudY,cloudMin,cloudMax
   };
 
   if(burjDemoOn==true) { //avvia la modalità scalata (climbOn viene impostato come true solo dopo la pressione del pulsante della squoia, per ora)
     //check_scal=true;
-    scelto=8;
-    climbMode(8,true,15,2,-550,400); //structNum,cloudBool,cloudX,cloudY,cloudMin,cloudMax
+    scelto=9;
+    climbMode(9,true,15,2,-550,400); //structNum,cloudBool,cloudX,cloudY,cloudMin,cloudMax
   };
 
   for (var i=0; i < myData.landmarks_en.length; i++){
@@ -222,7 +222,7 @@ if(backMenu==true) { //se true fa comparire il menu per tornare indietro
   text('Distanza Precedente: ' + metriPrec, 5, 30 * 6);
   text('conv: ' + conv, 5, 30 * 7);
   text('heading: ' + heading, 5, 30 * 8);
-  text('versione 17:34 3/02/18', 5, 30 * 9);
+  text('versione 12:02 3/02/18', 5, 30 * 9);
   pop();
   // console.log('infoOn: '+infoOn);
   // console.log('infoButtonShow: '+infoButtonShow);
@@ -1306,7 +1306,7 @@ function showLocation(position) {
 
         // console.log(conv);
        if ((stabilizzato==true)&&(metriTOT<myData.landmarks_en[scelto].height)&&(metriPrec>accuracyLimit)&&check_scal==true) {
-          if((head_scal==null && heading!=null) || (conta_head<7 && heading!=null)){
+          if((head_scal==null && heading!=null) || (conta_head<5 && heading!=null)){
              head_scal=heading;
              heading_tot=head_scal;
              conta_head++;
